eb64661dc9d9d2571c0b2d484895cef5ebfe6bf5
sarah1446 <lemonview2@gmail.com>
2019-03-31 10:02:22 +0900

    ADD - homework

--- src/01-basic/basic.js
+++ src/01-basic/basic.js
@@ -1,1 +1,19 @@
-export default function sum() {}
+export default function sum(x, y) {
+    var type  = x || y;
+    
+    if (typeof (type) === 'number') {
+        return x + y;
+    }else{
+        return '숫자를 넣어주세요';
+    }
+    
+    // if (typeof (type) === 'number') {
+    //     return x + y;
+    // }else if(typeof (type) === 'string'){
+    //     return '숫자를 넣어주세요';
+    // }else if(typeof(type) === 'boolean'){
+    //     return '숫자를 넣어주세요';'
+    // }else if(typeof(type) === 'array'){
+    //     return '숫자를 넣어주세요'
+    // }
+}
---
+++ src/test/test.spec.js
@@ -0,0 +1,7 @@
+import {plus} from './test.js';
+
+describe('대주제',function(){
+   it('조건', function(){
+       expect(plus(1, 2)).toBe(3);
+   }); 
+});
