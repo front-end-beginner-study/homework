01febbb63488fc15bb50a78fb2bf5e7a378d06f1
Kimchunyong <lovemy5854@naver.com>
2019-04-01 19:10:15 +0900

    [edit] 타입체크 변수명 변경 및 조건문 변경

--- src/01-basic/basic.js
+++ src/01-basic/basic.js
@@ -1,7 +1,7 @@
 export default function sum(...argArr) {
   const totalSum = argArr.reduce((total, cur) => total + cur, 0);
-  const typeChk = (typeItem) => isNaN(typeItem) || typeof typeItem === 'boolean';
-  const [chk] = argArr.map((typeItem) => (typeChk(typeItem) ? true : false));
+  const chkType = (typeItem) => isNaN(typeItem);
+  const [chk] = argArr.map((typeItem) => (chkType(typeItem) ? true : false));
 
   return chk ? '숫자를 넣어주세요' : totalSum;
 }
