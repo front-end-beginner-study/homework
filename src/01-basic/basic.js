e097ad89be66610a26ca9a9137c0acc1b180c953
Kimchunyong <lovemy5854@naver.com>
2019-04-01 16:47:02 +0900

    [update] code 정리

--- src/01-basic/basic.js
+++ src/01-basic/basic.js
@@ -1,13 +1,7 @@
-export default function sum (a, b) {
-  const argArr = [...arguments];
-  const totalSum = argArr.reduce((acc, cur) => {
-    return acc + cur;
-  }, 0);
-  const [chk] = argArr.map(argArr => {
-    if (isNaN(argArr) || typeof argArr === 'boolean') return true;
-  });
-  if (chk) {
-    return '숫자를 넣어주세요';
-  }
-  return totalSum;
+export default function sum(...argArr) {
+  const totalSum = argArr.reduce((total, cur) => total + cur, 0);
+  const typeChk = (typeItem) => isNaN(typeItem) || typeof typeItem === 'boolean';
+  const [chk] = argArr.map((typeItem) => (typeChk(typeItem) ? true : false));
+
+  return chk ? '숫자를 넣어주세요' : totalSum;
 }
