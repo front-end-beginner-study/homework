ca465500bed6cf15c4f877f95017d5aef5a00a7e
ryuree <rido94@naver.com>
2019-04-06 20:41:40 +0900

    [EDIT]basic

--- src/01-basic/basic.js
+++ src/01-basic/basic.js
@@ -2,16 +2,8 @@ export default function sum() {
     let output = 0;
 
     for (let i = 0; i < arguments.length; i++) {
-        output += arguments[i];
+        const isNum = typeof(arguments[i]) === 'number'
+        isNum ? output += arguments[i] : output = "숫자를 넣어주세요"
     }
-
-    const isNum = typeof(output) === 'number';
-
-    return isNum ? output : '숫자를 넣어주세요';
-
-	// if(isNum){
-    //     return output;
-    // }else{
-    //     return '숫자를 넣어주세요';
-    // }
+	return output;
 }
