4dd6a3aa8a4525b15e49cf3f426ca93d49b9c675
kimchunyong <lovemy5854@naver.com>
2019-04-14 12:52:29 +0900

    [add] counter 테스트

---
+++ src/02-counter/counter.js
@@ -0,0 +1,26 @@
+export default class Counter {
+  constructor (count = 0) {
+    this.count = typeof count === 'number' ? count : 0;
+    this.WRONG_MESSAGE = '숫자만 넣어주세요';
+  }
+
+  isNumber (num) {
+    return typeof num === 'number';
+  }
+
+  increment (incNum = 1) {
+    return this.isNumber(incNum) ? (this.count += incNum) : this.WRONG_MESSAGE;
+  }
+
+  decrement (decNum = 1) {
+    return this.isNumber(decNum) ? (this.count -= decNum) : this.WRONG_MESSAGE;
+  }
+
+  getCount () {
+    return this.count;
+  }
+
+  reset (resNum = 0) {
+    return (this.count = resNum);
+  }
+}
