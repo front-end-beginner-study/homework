f7874fa21aae8ba148fcc6b02d51de7cfb2a7d6d
zansol <hahn0406@gmail.com>
2019-04-27 23:59:31 +0900

    Edit [class]counter

--- src/02-counter/counter.js
+++ src/02-counter/counter.js
@@ -1,19 +1,18 @@
-import { isNumber } from 'util';
-
 export default class Counter {
   constructor(count = 0) {
-    this.count = typeof count === 'number' ? count : 0;
+    this.count = this.isNumber(count) ? count : 0;
+  }
+  isNumber(input) {
+    return typeof input === 'number';
   }
   getCount() {
     return this.count;
   }
-  increment(num = 0) {
-    typeof num === 'number' ? (this.count = this.count + 1) : '숫자만 넣어주세요';
-    // this.count = this.count + 1;
+  increment(num = 1) {
+    return this.isNumber(num) ? (this.count += num) : '숫자만 넣어주세요';
   }
   decrement(num = 1) {
-    typeof num === 'number' ? (this.count = this.count - num) : '숫자만 넣어주세요';
-    // this.count = this.count - num;
+    return this.isNumber(num) ? (this.count -= num) : '숫자만 넣어주세요';
   }
   reset(num = 0) {
     this.count = num;
